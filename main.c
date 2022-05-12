8dbeeba5c064ec252af350dcf789ab1fa627be68
rthome-d <rthome-d@student.42.rio>
2022-05-11 21:24:41 -0300

    ft_isascii.c ft_isprint.c

--- main.c
+++ main.c
@@ -3,6 +3,8 @@
 int ft_isalpha(int c);
 int ft_isdigit(int c);
 int ft_isalnum(int c);
+int ft_isascii(int c);
+int ft_isprint(int c);
 
 int main(void)
 {
@@ -16,6 +18,20 @@ int main(void)
 	printf("ft_isdigit(alpha) = %d\n\n", ft_isdigit(alpha));
 	printf("ft_isalnum(num) = %d\n", ft_isalnum(num));
 	printf("ft_isalnum(alpha) = %d\n\n", ft_isalnum(alpha));
-	
+	printf("ft_isascii(alph) = %d\n", ft_isascii(alpha));
+	printf("ft_isascii(num) = %d\n", ft_isascii(num));
+
+	int nptr = 1;
+	int sbl = '!'; 
+	int outascii = 128;
+	printf("ft_isascii(notPrintable) = %d\n", ft_isascii(nptr));
+	printf("ft_isascii(simbol) = %d\n", ft_isascii(sbl));	
+	printf("ft_isascii(outAscii) = %d\n\n", ft_isascii(outascii));
+
+	printf("ft_isprint(notPrintable) = %d\n", ft_isprint(nptr));
+	printf("ft_isprint(simbol) = %d\n", ft_isprint(sbl));
+	printf("ft_isprint(num) = %d\n", ft_isprint(num));
+	printf("ft_isprint(alpha) = %d\n", ft_isprint(alpha));
+	printf("ft_isprint(aoutAscii) = %d\n\n", ft_isprint(outascii));
 }
 
